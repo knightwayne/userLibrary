dc1ac1b7c2d1b598e6c06a0035bcf839d633a156
knightwayne <arnav7ishaan@gmail.com>
2022-11-08 11:46:13 -0500

    delete record function added

--- src/apiData/MainApp.java
+++ src/apiData/MainApp.java
@@ -382,15 +382,34 @@ public class MainApp{
 			System.out.println("SQL: " + sql);
 			stmt.executeUpdate(sql);
 
-			// ResultSet rs = stmt.executeQuery(sql);
-			
-			// int cnt=1;
-			// while ( rs.next() ) {
-			// 	String q = rs.getString(query);
-			// 	System.out.println("For Column " + cnt + ": " + "Value " + q);
-			// 	cnt++;
-			// }
-			// rs.close();
+			stmt.close();
+			c.commit();
+			c.close();
+		}
+		catch (Exception e)
+		{
+            e.printStackTrace();
+        }
+	}
+
+	public static void deleteData(String table, String lhs, String rhs)
+	{
+		try
+		{
+			Connection c = null;
+			Statement stmt = null;
+			String sql="";
+			//Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:testDB.db");
+			c.setAutoCommit(false);
+			System.out.println("Opened database successfully");
+			stmt = c.createStatement();
+
+			sql = "DELETE FROM " + table + " WHERE " + lhs + " = " + rhs + ";" ;
+			// UPDATE BOOKREVIEW set RATING = 88 where Date=ww2;
+			// UPDATE COMPANY set SALARY = 25000.00 where ID=1;
+			System.out.println("SQL: " + sql);
+			stmt.executeUpdate(sql);
 
 			stmt.close();
 			c.commit();
@@ -400,12 +419,6 @@ public class MainApp{
 		{
             e.printStackTrace();
         }
-	
-	}
-
-	public static void deleteData(String table, String query)
-	{
-
 	}
 	//	4. Main App Switch-Case Loop
 	public static void main(String[] args) throws SQLException, SQLWarning
@@ -466,6 +479,11 @@ public class MainApp{
 				case 4:
 				{
 					System.out.println("Delete From Table");
+					String Table=reader.next();
+					String LHS=reader.next();
+					String RHS=reader.next();
+					//System.out.println(Table+Query);
+					deleteData(Table, LHS, RHS);
 					//break;
 				}
 				break;
